79422d19d3dc4a15efd11ffd86011e6e7782aaac
Parth Mittal <mittalparth22@gmail.com>
2023-12-09 02:47:45 +0530

    Contract: Add CRUD for other structs

--- contracts/Resource.sol
+++ contracts/Resource.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Resource {
     using Counters for Counters.Counter;
-    
+
     enum ComputeResourceStatus {
         FREE,
         IN_USE
@@ -105,4 +105,148 @@ contract Resource {
         );
         modelObjectsById[newId] = newModel;
     }
+
+    function createComputeResource(
+        string memory _title,
+        uint256 _ram,
+        uint256 _gpu,
+        string memory _descriptionAddress,
+        uint256 _score,
+        uint256 _ratePerMin,
+        bool _isActive
+    ) public {
+        computeResourceIdCounter.increment();
+        uint256 newId = computeResourceIdCounter.current();
+
+        computeResourceObjectsById[newId] = ComputeResource(
+            newId,
+            _title,
+            _ram,
+            _gpu,
+            _descriptionAddress,
+            payable(msg.sender),
+            _score,
+            _ratePerMin,
+            _isActive,
+            ComputeResourceStatus.FREE
+        );
+    }
+
+    function createJob(uint256 _minScorePerComputeResource, uint256 _computeResourceCount, uint256 _maxRatePerMin) public {
+        jobIdCounter.increment();
+        uint256 newId = jobIdCounter.current();
+
+        uint256[] memory emptyArray; // Empty array of compute resource IDs
+        jobObjectsById[newId] = Job(
+            newId,
+            emptyArray,
+            payable(msg.sender),
+            _minScorePerComputeResource,
+            _computeResourceCount,
+            _maxRatePerMin,
+            0,
+            JobStatus.INITIALISED
+        );
+    }
+
+    // Function to add compute resource ID to a Job
+    function addComputeResourceIdToJob(
+        uint256 _jobId,
+        uint256 _computeResourceId
+    ) public {
+        Job storage job = jobObjectsById[_jobId];
+        // require(job.owner == msg.sender, "Not the owner of the job");
+        require(
+            job.computeResourceIds.length < 50,
+            "Exceeded maximum array length"
+        );
+        job.computeResourceIds.push(_computeResourceId);
+    }
+
+    // Functions to retrieve objects by their id
+    function getDataDetails(uint256 _id) public view returns (Data memory) {
+        return dataObjectsById[_id];
+    }
+
+    function getModelDetails(uint256 _id) public view returns (Model memory) {
+        return modelObjectsById[_id];
+    }
+
+    function getComputeResourceDetails(
+        uint256 _id
+    ) public view returns (ComputeResource memory) {
+        return computeResourceObjectsById[_id];
+    }
+
+    function getJobDetails(uint256 _id) public view returns (Job memory) {
+        return jobObjectsById[_id];
+    }
+
+    // Function to fetch Data objects for msg.sender
+    function fetchMyData() public view returns (Data[] memory) {
+        uint count = dataIdCounter.current();
+        Data[] memory myData = new Data[](count);
+
+        for (uint i = 1; i <= count; i++) {
+            Data storage data = dataObjectsById[i];
+            if (data.owner == msg.sender) {
+                myData[i - 1] = data;
+            }
+        }
+
+        return myData;
+    }
+
+    // Function to fetch Model objects for msg.sender
+    function fetchMyModels() public view returns (Model[] memory) {
+        uint count = modelIdCounter.current();
+        Model[] memory myModels = new Model[](count);
+
+        for (uint i = 1; i <= count; i++) {
+            Model storage model = modelObjectsById[i];
+            if (model.owner == msg.sender) {
+                myModels[i - 1] = model;
+            }
+        }
+
+        return myModels;
+    }
+
+    // Function to fetch ComputeResource objects for msg.sender
+    function fetchMyComputeResources()
+        public
+        view
+        returns (ComputeResource[] memory)
+    {
+        uint count = computeResourceIdCounter.current();
+        ComputeResource[] memory myComputeResources = new ComputeResource[](
+            count
+        );
+
+        for (uint i = 1; i <= count; i++) {
+            ComputeResource
+                storage computeResource = computeResourceObjectsById[i];
+            if (computeResource.owner == msg.sender) {
+                myComputeResources[i - 1] = computeResource;
+            }
+        }
+
+        return myComputeResources;
+    }
+
+    // Function to fetch Job objects for msg.sender
+    function fetchMyJobs() public view returns (Job[] memory) {
+        uint count = jobIdCounter.current();
+        Job[] memory myJobs = new Job[](count);
+
+        for (uint i = 1; i <= count; i++) {
+            Job storage job = jobObjectsById[i];
+            if (job.owner == msg.sender) {
+                myJobs[i - 1] = job;
+            }
+        }
+
+        return myJobs;
+    }
+
 }
