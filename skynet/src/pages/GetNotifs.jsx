383277b202316f720b15ffe68db8cb0852a93799
kid-116 <mehultodi116@gmail.com>
2023-12-10 05:15:03 +0530

    Enable user to accept/reject a job request

--- skynet/src/pages/GetNotifs.jsx
+++ skynet/src/pages/GetNotifs.jsx
@@ -3,8 +3,11 @@ import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
 import { useWalletClient } from "wagmi";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Routes, Route, useNavigate } from "react-router-dom";
-
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useContractWrite } from "wagmi";
+import { CONTRACT_ADDRESS } from "../config";
+import ABI from "../constants/skynetabi.json";
+const skynetABI=ABI.abi;
 // PushAPI.initialize(signer, {options?});
 // signer - pass the signer from your app and set env to 'CONSTANTS.ENV.PROD' for mainnet app
 // options? - optional, can pass initialization parameters for customization
@@ -14,9 +17,10 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 
 export default function GetNotifs() {
   const navigate = useNavigate();
-  const [notifs, setNotifs] = useState([]);
+  const [notifs, setNotifs] = useState(() => new Set());
+  // const [jobRequests, setJobRequests] = useState(() => new Set())
 
-  const { data: walletClient, isError, isLoading } = useWalletClient();
+  const { data: walletClient, isError } = useWalletClient();
 
   async function fetchNotifs() {
     const user = await PushAPI.initialize(walletClient, {
@@ -29,10 +33,40 @@ export default function GetNotifs() {
     sample = sample.map((notif) => {
       return { message: notif.message, title: notif.title };
     });
-    setNotifs(sample);
+    setNotifs((notifs) => {
+      const _notifs = new Set(notifs)
+      _notifs.add(sample)
+      return _notifs
+    });
   }
   // const { data: signer } = useSigner();
 
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    address: CONTRACT_ADDRESS,
+    abi: skynetABI,
+    functionName: 'addComputeResourceIdToJob',
+  })
+
+  useEffect(() => {
+    console.log(data, isError, isSuccess)
+  }, [isLoading, isSuccess, data])
+
+  const signUpForJob = (jobId) => {
+    console.log("Signing up ...")
+    write({
+      args: [jobId, walletClient.account.address],
+      from: walletClient.account.address
+    })
+  }
+
+  const rejectJob = (notif) => {
+    setNotifs((notifs) => {
+      const _notifs = new Set(notifs)
+      _notifs.delete(notif)
+      return _notifs 
+    })
+  }
+  
   useEffect(() => {
     if (walletClient) fetchNotifs();
   }, [walletClient]);
@@ -40,7 +74,7 @@ export default function GetNotifs() {
     <div className="bg-gray-900 min-h-screen p-8 text-white flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-8">Notifications</h1>
       <div className="space-y-4 w-full max-w-sm">
-        {notifs.map((notif, index) => (
+        {Array.from(notifs).map((notif, index) => (
           <div
             key={index}
             className="bg-gray-800 rounded-lg overflow-hidden shadow-lg"
@@ -50,10 +84,13 @@ export default function GetNotifs() {
               <p className="text-gray-300">{notif.message}</p>
             </div>
             <div className="bg-gray-700 p-3 flex justify-between">
-              <button className="bg-green-600 hover:bg-green-500 text-white font-bold py-1 px-3 rounded transition duration-150">
+              <button className="bg-green-600 hover:bg-green-500 text-white font-bold py-1 px-3 rounded transition duration-150"
+              onClick={() => {signUpForJob(notif.message)}}>
                 Accept
               </button>
-              <button className="bg-red-600 hover:bg-red-500 text-white font-bold py-1 px-3 rounded transition duration-150">
+              <button className="bg-red-600 hover:bg-red-500 text-white font-bold py-1 px-3 rounded transition duration-150"
+              onClick={() => {rejectJob(notif)}}
+              >
                 Reject
               </button>
             </div>
