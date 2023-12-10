57235354fee08eed6851b08361f723493b5ad372
abhiraj-mengade <abhiraj.mengade@outlook.com>
2023-12-10 07:10:56 +0530

    Fix job config

--- skynet/src/pages/JobConfig.jsx
+++ skynet/src/pages/JobConfig.jsx
@@ -1,40 +1,45 @@
 import React, { useState } from "react";
 import Form from "../components/Form";
-import { useWalletClient, useSignMessage,useContractWrite } from "wagmi";
+import { useWalletClient, useSignMessage, useContractWrite } from "wagmi";
 import FormPage from "../components/FormPage";
 import { useLocation } from "react-router-dom";
 import { CONTRACT_ADDRESS } from "../config";
 import ABI from "../constants/skynetabi.json";
-const skynetABI=ABI.abi;
+const skynetABI = ABI.abi;
 
 export default function JobConfig() {
   const location = useLocation();
-  console.log(location.state)
-  const hashList = location.state?? [];
+  console.log(location.state);
+  const hashList = location.state ?? [];
   const [jobDetails, setJobDetails] = useState({});
-  const { data: walletClient} = useWalletClient();
+  const { data: walletClient } = useWalletClient();
 
   const { data, isLoading, isSuccess, write } = useContractWrite({
     address: CONTRACT_ADDRESS,
     abi: skynetABI,
-    functionName: 'createJob',
-  })
+    functionName: "createJob",
+  });
 
   const handleSubmit = async (filehash) => {
-    console.log("Create Job")
-    console.log(jobDetails)
+    console.log("Create Job");
+    console.log(jobDetails);
     write({
-      args: [jobDetails.minScorePerComputeResource,jobDetails.computeResourceCount,jobDetails.maxRatePerMin],
-      from: walletClient.account.address
-    })
-    console.log("Job Created")
+      args: [
+        jobDetails.minScorePerComputeResource,
+        jobDetails.computeResourceCount,
+        jobDetails.maxRatePerMin,
+        [],
+      ],
+      from: walletClient.account.address,
+    });
+    console.log("Job Created");
   };
 
   return (
     <FormPage
       form={
         <Form
-        walletClient = {walletClient}
+          walletClient={walletClient}
           handleSubmit={handleSubmit}
           fields={[
             {
